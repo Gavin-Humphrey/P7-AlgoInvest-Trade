9f71ea4027e5a266afb03012e41abd767a1d6dc5
GmGith <87491753+GmGith@users.noreply.github.com>
2022-09-28 17:56:55 +0200

    added file - option 2

---
+++ knapsack.py
@@ -0,0 +1,110 @@
+import csv
+from tqdm import tqdm
+import time
+
+
+
+start_time = time.time()
+MAX_INVEST = 500
+
+
+def main():
+
+    filename = "data/dataset1.csv"#
+    actions_list = read_csv(filename)
+    print(f"\nComputing {len(actions_list)} shares for {MAX_INVEST}€ :")
+    show_cost_profit(knapSack(actions_list))
+  
+
+def read_csv(filename):
+    """Import shares data from csv file
+    check rows and filter out corrupted data 
+    Then return list of shares data 
+    """
+    with open(filename) as csvfile:
+        actions_file = csv.reader(csvfile, delimiter=',')
+
+        actions_list = []
+
+        for row in actions_file:
+            if float(row[1]) <= 0 or float(row[2]) <= 0:
+                pass
+            else:
+                action = (
+                    row[0],
+                    int(float(row[1])*100),
+                    float(float(row[1]) * float(row[2]) / 100)
+                )
+                actions_list.append(action)
+
+        return actions_list
+
+    
+def knapSack(actions_list):
+    """Sort the actions_list
+    Knapsack - Initialize the matrix  - k
+    Retrieve the best combination of shares
+    actions_list - list of shares - as parameter
+    Returns the best possible combinations list
+    max_inv = capacity
+    cost = weights
+    profit = values
+    """
+    
+    max_inv = MAX_INVEST * 100   
+    total_action = len(actions_list)
+    cost = []      
+    profit = []    
+
+    for action in actions_list:
+        cost.append(action[1])
+        profit.append(action[2])
+
+    # Finding the best value (profit)
+    k = [[0 for x in range(max_inv +1)] for x in range(total_action + 1)] # Making the k array
+  
+    for i in tqdm(range(1, total_action+1)):  # taking first i elements
+        for w in range(1, max_inv + 1):  
+            if cost[i-1] <= w:    
+                k[i][w] = max(profit[i-1] + k[i-1][w-cost[i-1]], k[i-1][w]) # finding the maximum value(profit)
+            else:
+                k[i][w] = k[i-1][w]
+     
+
+    #  Getting actions combination from best value (profits)
+    best_combi = []
+
+    while max_inv >= 0 and total_action >= 0:
+
+        if k[total_action][max_inv] == \
+                k[total_action-1][max_inv - cost[total_action-1]] + profit[total_action-1]:
+
+            best_combi.append(actions_list[total_action-1])
+            max_inv -= cost[total_action-1]
+
+        total_action -= 1
+
+    return best_combi  # returning the maximum value (profit) combination of knaps
+
+
+def show_cost_profit(best_combi):
+    """Show best cost and prifit combinations result
+    The list of most profitable shares combination - best_combi - as param
+    """
+    best_combi.sort(key=lambda action: action[2], reverse=True)
+    print(f"\nMost profitable investments ({len(best_combi)}  shares) :\n")
+
+    cost = []
+    profit = []
+
+    for item in best_combi:
+        print(f"{item[0]} | {item[1] / 100} € | +{item[2]} €")
+        cost.append(item[1] / 100)
+        profit.append(item[2])
+
+    print("\nTotal cost : ", sum(cost), "€")
+    print("Profit after 2 years : +", sum(profit), "€")
+    print("\nTime taken : ", time.time() - start_time, "seconds\n")
+
+if __name__ == "__main__":
+    main()
--- optimized.py
+++ optimized.py
@@ -1,109 +1,83 @@
 import csv
-from tqdm import tqdm
+from dataclasses import dataclass
+from time import perf_counter
 import time
 
 
 
+ 
 start_time = time.time()
-MAX_INVEST = 500
+
+@dataclass(frozen=True)
+class Action:
+    """Represents a action"""
+    name: str
+    cost: float
+    percent: float
+    
+
+    @property
+    def profit(self) -> float:
+        """Compute a return profit from cost and percent"""
+        return self.cost * self.percent / 100
+
+    def __str__(self):
+        return f"{self.name} | cost: {self.cost}€ | percent: {self.percent}% | profit: {self.profit:.2f}€"
+
+    def __lt__(self, other_action: "Action"):
+        """Allows to sort list of Action on percent profit"""
+        return self.percent < other_action.percent
+
+def get_actions_objects_from_csv(file_name: str) -> list[Action]:
+    """Return a list of Action object from a csv file"""
+    actions = []
+    with open(file_name, newline="") as csv_File:
+        reader = csv.reader(csv_File, delimiter=",")
+        for row in reader:
+            cost = float(row[1])
+            percent = float(row[2])
+            if cost <= 0.0 or percent <= 0:
+                continue
+
+            action = Action(row[0], cost, percent)
+            actions.append(action)
+
+    return actions
+
+
+
+def best_cost_pro(actions: list[Action]):
+    """Returns the best actions with total invest and total profit"""
+    actions.sort(reverse=True)
+    max_invest = 500
+    total_profit: float = 0
+    total_invest: float = 0
+    best_actions = []
+
+    for action in actions:
+        if action.cost <= max_invest:
+            max_invest -= action.cost
+            total_invest += action.cost
+            total_profit += action.profit
+            best_actions.append(action)
+
+    return best_actions, total_invest, total_profit
 
 
 def main():
 
-    filename = "data/dataset1.csv"#
-    actions_list = read_csv(filename)
-    print(f"\nComputing {len(actions_list)} shares for {MAX_INVEST}€ :")
-    show_cost_profit(knapSack(actions_list))
-  
+    actions = get_actions_objects_from_csv("data/dataset1.csv")
+    best_actions, invest, profit = best_cost_pro(actions)
+    print("")
+    print(f"Computed {len(actions)} Shares")
+    print("")
+    print(f"Total Invest: {invest:.2f}€\nTotal Profit: {profit:.2f}€\nN° Of Most Profitable Shares: {len(best_actions)}")
+    print("")
+    for action in best_actions:
+        print(action)
 
-def read_csv(filename):
-    """Import shares data from csv file
-    check rows and filter out corrupted data 
-    Then return list of shares data 
-    """
-    with open(filename) as csvfile:
-        actions_file = csv.reader(csvfile, delimiter=',')
-
-        actions_list = []
-
-        for row in actions_file:
-            if float(row[1]) <= 0 or float(row[2]) <= 0:
-                pass
-            else:
-                action = (
-                    row[0],
-                    int(float(row[1])*100),
-                    float(float(row[1]) * float(row[2]) / 100)
-                )
-                actions_list.append(action)
-
-        return actions_list
-
-    
-def knapSack(actions_list):
-    """Sort the actions_list
-    Knapsack - Initialize the matrix  - k
-    Retrieve the best combination of shares
-    actions_list - list of shares - as parameter
-    Returns the best possible combinations list
-    max_inv = capacity
-    cost = weights
-    profit = values
-    """
-    actions_list.sort(key=lambda action: action[2], reverse=False)
-    max_inv = MAX_INVEST * 100   
-    total_action = len(actions_list)
-    cost = []      
-    profit = []    
-
-    for action in actions_list:
-        cost.append(action[1])
-        profit.append(action[2])
-
-    # Finding the best value (profit)
-    k = [[0 for x in range(max_inv +1)] for x in range(total_action + 1)] # Making the k array
-
-    for i in tqdm(range(1, total_action+1)):  # taking first i elements
-        for w in range(1, max_inv + 1):  
-            if cost[i-1] <= w:    
-                k[i][w] = max(profit[i-1] + k[i-1][w-cost[i-1]], k[i-1][w]) # finding the maximum value(profit)
-            else:
-                k[i][w] = k[i-1][w]
-     
-
-    #  Getting actions combination from best value (profits)
-    best_combi = []
-
-    while max_inv >= 0 and total_action >= 0:
-
-        if k[total_action][max_inv] == \
-                k[total_action-1][max_inv - cost[total_action-1]] + profit[total_action-1]:
-
-            best_combi.append(actions_list[total_action-1])
-            max_inv -= cost[total_action-1]
-
-        total_action -= 1
-
-    return best_combi  # returning the maximum value (profit) combination of knaps
+    print("\nTime elapsed : ", time.time() - start_time, "seconds\n")
 
 
-def show_cost_profit(best_combi):
-    """Show best cost and prifit combinations result
-    The list of most profitable shares combination - best_combi - as param
-    """
-    print(f"\nMost profitable investments ({len(best_combi)}  shares) :\n")
-
-    cost = []
-    profit = []
-
-    for item in best_combi:
-        print(f"{item[0]} | {item[1] / 100} € | +{item[2]} €")
-        cost.append(item[1] / 100)
-        profit.append(item[2])
-
-    print("\nTotal cost : ", sum(cost), "€")
-    print("Profit after 2 years : +", sum(profit), "€")
-    print("\nTime taken : ", time.time() - start_time, "seconds\n")
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+   main()
